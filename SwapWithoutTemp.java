d60d7ba87c9d2d21c1a6fb0054841a2b5dcfc92a
Huyentrang03789 <duongthtrang06@gmail.com>
2025-09-30 18:16:52 +0700

    Add files via upload

---
+++ SwapWithoutTemp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SwapWithoutTemp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số nguyên a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Nhập số nguyên b: ");
+        int b = scanner.nextInt();
+
+        System.out.println("Trước khi hoán đổi: a = " + a + ", b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("Sau khi hoán đổi: a = " + a + ", b = " + b);
+    }
+}
