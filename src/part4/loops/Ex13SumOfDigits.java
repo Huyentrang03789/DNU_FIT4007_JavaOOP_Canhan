2a8a30854fc4f1c21e1469897a292d971d67be55
Huyentrang03789 <duongtrang2666@gmail.com>
2025-10-14 23:18:16 +0700

    feat:update ex10 to ex13 part 4

---
+++ src/part4/loops/Ex13SumOfDigits.java
@@ -0,0 +1,15 @@
+package part4.loops;
+import java.util.Scanner;
+
+public class Ex13SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int sum = 0;
+        while (N > 0) {
+            sum += N % 10;
+            N /=10;
+        }
+        System.out.println("Tổng các chữ số = " +sum);
+    }
+}
