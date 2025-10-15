4f366a7e61ce5cb5cf509b1e36b68b4cfa5a8457
Huyentrang03789 <duongtrang2666@gmail.com>
2025-10-15 12:16:26 +0700

    feat:update ex14 to ex15 part 4

---
+++ src/part4/loops/Ex14PrintEachCharacter.java
@@ -0,0 +1,12 @@
+package part4.loops;
+import java.util.Scanner;
+
+public class Ex14PrintEachCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập tên của bạn: ");
+        String name = sc.nextLine();
+        for (int i = 0; i < name.length(); i++)
+            System.out.println(name.charAt(i));
+    }
+}
