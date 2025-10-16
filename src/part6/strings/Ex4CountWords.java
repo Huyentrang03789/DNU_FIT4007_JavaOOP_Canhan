587a192bdc153531b321d25c27900c8f38cf61eb
Huyentrang03789 <duongtrang2666@gmail.com>
2025-10-16 18:49:16 +0700

    feat:update ex4 to ex6 part 6

---
+++ src/part6/strings/Ex4CountWords.java
@@ -0,0 +1,17 @@
+package part6.strings;
+import java.util.Scanner;
+
+public class Ex4CountWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập chuỗi: ");
+        String str = sc.nextLine().trim();
+
+        if (str.isEmpty()) {
+            System.out.println("Số từ: 0");
+        } else {
+            String[] words = str.split("\\s+");
+            System.out.println("Số từ trong chuỗi là: " + words.length);
+        }
+    }
+}
