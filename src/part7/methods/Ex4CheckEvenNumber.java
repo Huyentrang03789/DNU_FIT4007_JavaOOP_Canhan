b48aff17e37a47922caea2572d2e0dad79caa706
Huyentrang03789 <duongtrang2666@gmail.com>
2025-10-17 00:30:01 +0700

    feat:update ex4 to ex6 part 7

---
+++ src/part7/methods/Ex4CheckEvenNumber.java
@@ -0,0 +1,11 @@
+package part7.methods;
+
+public class Ex4CheckEvenNumber {
+    static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEven(6));
+    }
+}
---
+++ src/part7/methods/Ex5CalculateFactorial.java
@@ -0,0 +1,13 @@
+package part7.methods;
+
+public class Ex5CalculateFactorial {
+    static long factorial(int n) {
+        long f = 1;
+        for (int i = 1; i <= n; i++) f *= i;
+        return f;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
