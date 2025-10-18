7f9c72497e9806e8276874231b975553028fd4c7
Huyentrang03789 <duongtrang2666@gmail.com>
2025-10-19 03:08:34 +0700

    feat:update ex7 to ex8 oop1

---
+++ src/oop1/constructors/Ex7CarMain.java
@@ -0,0 +1,8 @@
+package oop1.constructors;
+
+public class Ex7CarMain {
+    public static void main(String[] args) {
+        Ex7Car car = new Ex7Car("Red", "Toyota", 4,2024);
+        System.out.println(car);
+    }
+}
---
+++ src/oop1/constructors/Ex8ProductMain.java
@@ -0,0 +1,8 @@
+package oop1.constructors;
+
+public class Ex8ProductMain {
+    public static void main(String[] args) {
+        Ex8Product p = new Ex8Product("Laptop", 20000, 5);
+        System.out.println(p);
+    }
+}
